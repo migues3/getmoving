9fb9aa49491e8544f237d97dcb9157c5d6db2598
Miguel <migues3@uci.edu>
2021-05-14 20:07:53 -0700

    created simple backend and frontend for step tracking chart

--- server.py
+++ server.py
@@ -1,19 +1,35 @@
-from flask import Flask
-from flask import request
-from flask import Response
+from flask import Flask, request, Response, render_template
 import json
+import time
 from datetime import datetime
 
 app = Flask(__name__)
+numSteps = 0
+previousNumSteps = -1
 
 @app.route("/")
-def hello():
-   def send_data():
-       while True:
-           json_data = json.dumps(
-               {'time': datetime.now().strftime('%Y-%m%d %H:%M:%S'), 'value': request.args.get("temp")}
-           yield f"data: {json_data}\n\n"
-           time.sleep(1)
+def home():
+    return render_template("index.html")
+
+def send_data():
+   global numSteps 
+   global previousNumSteps
+   while True:
+       if numSteps != previousNumSteps:
+        json_data = json.dumps({'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': numSteps})
+        yield f"data: {json_data}\n\n"
+        previousNumSteps = numSteps
+
+@app.route("/track-steps")
+def track():
+   global numSteps 
+   numSteps = request.args.get("steps")
+
+   if numSteps is None:
+       numSteps = 0 
    
    return Response(send_data(), mimetype='text/event-stream')  
    #return "Temperature: " + str(request.args.get("temp")) + " Humidity: " + str(request.args.get("humidity"))
+
+if __name__ == "__main__":
+    app.run()
