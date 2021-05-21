73e1fa00828d7fd58a31a0ce9e677f1687bb906c
Miguel <migues3@uci.edu>
2021-05-20 17:55:34 -0700

    Added backend and frontend for idle tracking

--- server.py
+++ server.py
@@ -6,16 +6,40 @@ from datetime import datetime
 app = Flask(__name__)
 numSteps = None
 previousNumSteps = None
+stepsPerDay = {}
+idlePerDay = {}
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/my-steps")
+def my_steps():
+   if len(stepsPerDay) > 7:
+      keys = list(stepsPerDay.keys())
+      stepsPerDay.pop(keys[0], None)
+   
+   return render_template("my-steps.html", steps = stepsPerDay)
+
+@app.route("/my-idle")
+def my_idle():
+   if len(idlePerDay) > 7:
+      keys = list(idlePerDay.keys())
+      idlePerDay.pop(keys[0], None)
+
+   return render_template("my-idle.html", idle = idlePerDay)
+
 def send_steps():
    global numSteps 
    global previousNumSteps
+   global stepsPerDay
+
    while True:
        if numSteps != previousNumSteps:
+
+        day = datetime.now().strftime('%Y-%m-%d')
+        stepsPerDay[day] = numSteps
+
         json_data = json.dumps({'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': numSteps})
         yield "data: {}\n\n".format(json_data)
         previousNumSteps = numSteps
@@ -26,11 +50,15 @@ def track_steps():
    numSteps = request.args.get("steps")
    
    return Response(send_steps(), mimetype='text/event-stream')  
-   #return "Temperature: " + str(request.args.get("temp")) + " Humidity: " + str(request.args.get("humidity"))
 
 @app.route("/track-idle")
 def track_idle():
-   pass
+   idleNum = request.args.get("idleNum")
+   day = datetime.now().strftime('%Y-%m-%d')
+
+   idlePerDay[day] = idleNum 
+
+   return idleNum
 
 if __name__ == "__main__":
     app.run()
