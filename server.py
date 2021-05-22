8c44bb6e4bfcf38bbd8e29c839073135a7cda0b5
Miguel <migues3@uci.edu>
2021-05-21 18:55:40 -0700

    Now saving steps and idle tracking to a database

--- server.py
+++ server.py
@@ -1,13 +1,30 @@
 from flask import Flask, request, Response, render_template
+from flask_sqlalchemy import SQLAlchemy
 import json
 import time
 from datetime import datetime
 
 app = Flask(__name__)
-numSteps = None
-previousNumSteps = None
-stepsPerDay = {}
-idlePerDay = {}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class Steps(db.Model):
+   date = db.Column(db.Date, primary_key = True)
+   count = db.Column(db.Integer, nullable = False)
+
+   def __repr__(self):
+        return "Steps: {} {}".format(self.date, self.count)
+
+class Idle(db.Model):
+   date = db.Column(db.Date, primary_key = True)
+   count = db.Column(db.Integer, nullable = False)   
+
+   def __repr__(self):
+        return "Idle: {} {}".format(self.date, self.count)
+
+
+numSteps = 0
 
 @app.route("/")
 def home():
@@ -15,48 +32,75 @@ def home():
 
 @app.route("/my-steps")
 def my_steps():
-   if len(stepsPerDay) > 7:
-      keys = list(stepsPerDay.keys())
-      stepsPerDay.pop(keys[0], None)
+   stepsPerDay = {}
+
+   lastSeven = Steps.query.order_by(Steps.date).limit(7).all()
+   
+   for step in lastSeven:
+      stepsPerDay[str(step.date)] = step.count
    
    return render_template("my-steps.html", steps = stepsPerDay)
 
 @app.route("/my-idle")
 def my_idle():
-   if len(idlePerDay) > 7:
-      keys = list(idlePerDay.keys())
-      idlePerDay.pop(keys[0], None)
+   idlePerDay = {}
+
+   lastSeven = Idle.query.order_by(Idle.date).limit(7).all()
+
+   for idle in lastSeven:
+      idlePerDay[str(idle.date)] = idle.count
 
    return render_template("my-idle.html", idle = idlePerDay)
 
 def send_steps():
    global numSteps 
-   global previousNumSteps
-   global stepsPerDay
 
    while True:
-       if numSteps != previousNumSteps:
+      day = datetime.now().date()
+      stepsFromDB = Steps.query.filter_by(date = day).first()
 
-        day = datetime.now().strftime('%Y-%m-%d')
-        stepsPerDay[day] = numSteps
+      # If step count != the step count in database we must update database
+      if numSteps != stepsFromDB.count and numSteps is not None:
 
-        json_data = json.dumps({'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': numSteps})
-        yield "data: {}\n\n".format(json_data)
-        previousNumSteps = numSteps
+         stepsFromDB.count = numSteps
+         db.session.commit()
+
+         # Send steps to main chart
+         json_data = json.dumps({'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': numSteps})
+         yield "data: {}\n\n".format(json_data)
 
 @app.route("/track-steps")
 def track_steps():
-   global numSteps 
+   global numSteps
+
+   day = datetime.now().date()
+   stepsFromDB = Steps.query.filter_by(date = day).first()
+
+   # Check if already in database
+   if stepsFromDB is None:
+      db.session.add(Steps(date=day, count=0))
+      db.session.commit()
+
    numSteps = request.args.get("steps")
+   if numSteps is not None:
+      numSteps = int(numSteps)
    
    return Response(send_steps(), mimetype='text/event-stream')  
 
 @app.route("/track-idle")
 def track_idle():
    idleNum = request.args.get("idleNum")
-   day = datetime.now().strftime('%Y-%m-%d')
+   print("Idle num = ", idleNum)
+   day = datetime.now().date()
+   idleFromDB = Idle.query.filter_by(date = day).first()
+   print("idlefromdb =", idleFromDB)
 
-   idlePerDay[day] = idleNum 
+   if idleFromDB is None:
+      db.session.add(Idle(date=day, count=idleNum))
+      db.session.commit()
+   else:
+      idleFromDB.count = idleNum
+      db.session.commit()   
 
    return idleNum
 
